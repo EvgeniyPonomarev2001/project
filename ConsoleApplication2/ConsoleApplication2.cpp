d44138bb0b1eae57ad4df9c91868873dca3f40aa
EvgeniyPonomarev2001 <75800869+EvgeniyPonomarev2001@users.noreply.github.com>
2020-12-12 13:21:12 +0300

    project
    Изучить работу с Git и GitHub

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	int const t = 10;
+	int a[t], r = 1;
+	for (int i = 0; i < t; i++)
+	{
+		a[i] = rand() % 101;
+		cout << setw(3) << a[i];
+	}
+	for (int i = 0; i < t; i++)
+	{
+		if (a[i] / 3 == 0 || a[i] / 9 == 0)
+			r = r * a[i];
+	}
+	cout << endl;
+	cout  << r;
+	system("pause");
+	return 0;
+}
